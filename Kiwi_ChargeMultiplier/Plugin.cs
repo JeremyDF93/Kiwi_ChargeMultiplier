9f3c794498bca0d9b68ddabe9b3e1b2d14bc9d05
Jeremy Farmer <jeremydf10@gmail.com>
2025-03-16 03:51:19 -0700

    initial commit

---
+++ Kiwi_ChargeMultiplier/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace Kiwi_ChargeMultiplier;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    public static Plugin instance;
+    
+    internal ConfigEntry<float> configChargeMultiplier;
+
+    private void Awake()
+    {
+        Harmony.CreateAndPatchAll(typeof(Plugin));
+        
+        instance = this;
+        
+        configChargeMultiplier = Config.Bind("General", "ChargeMultiplier", 2.0f, "A charge multiplier given to items that make crystals more efficient. This applies to the energy drone as well.");
+    }
+    
+    [HarmonyPatch(typeof(ItemBattery), "ChargeBattery"), HarmonyPrefix]
+    static void ChargeBattery(ref float chargeAmount)
+    {
+        chargeAmount = chargeAmount * Plugin.instance.configChargeMultiplier.Value;
+    }
+}
